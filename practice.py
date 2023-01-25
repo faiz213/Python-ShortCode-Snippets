8caa232202a814b275e1a95a4dbde8d4737dbd2a
faiz213 <bsef21m554@pucit.edu.pk>
2023-01-25 20:46:58 +0500

    Addded:

--- practice.py
+++ practice.py
@@ -1,29 +1,29 @@
-# 1st programmes to pratice
+#1st programmes to pratice
 
-#print ('hello what is your name ?')
-#print ('what is your name : ')
-#myName = input()
-#print ('it is good to meet you ' + myName)
-#print ('what is your age ?')
-#myAge = input()
-#print ('oh cool you are still young ' + myName)
-#print ('you will be ' + str (int  (myAge) +1) + ' next year' ) 
+print ('hello what is your name ?')
+print ('what is your name : ')
+myName = input()
+print ('it is good to meet you ' + myName)
+print ('what is your age ?')
+myAge = input()
+print ('oh cool you are still young ' + myName)
+print ('you will be ' + str (int  (myAge) +1) + ' next year' ) 
 
 #2nd programme to practice
 
-#name = 'faiz'
-#if name == 'faiz':
- #   print ('hi faiz')
-#else:
- #   print('error')
+name = 'faiz'
+if name == 'faiz':
+   print ('hi faiz')
+else:
+   print('error')
 
 
-#password = 'ghotki123'
-#password = input()
-#if password == 'ghotki123':
- #   print('Access Granted! ')
-#else:
- #   print('Access Denied Wrong Password')
+password = 'ghotki123'
+password = input()
+if password == 'ghotki123':
+   print('Access Granted! ')
+else:
+   print('Access Denied Wrong Password')
 
 
 
--- practice3.py
+++ practice3.py
@@ -1,31 +1,31 @@
-  # While loop
-# name = ' '
-# while True:
-#     print('plz type your name')
-#     name = input()
-#     if name == 'faiz':
-#         break
-# print ('thank You')
+#  While loop
+name = ' '
+while True:
+    print('plz type your name')
+    name = input()
+    if name == 'faiz':
+        break
+print ('thank You')
 
-# spam = 0
-# while spam < 5:
-#     spam = spam+1
-#     if spam == 3:
-#         continue
-# print ('spam is ' + str(spam))    
+spam = 0
+while spam < 5:
+    spam = spam+1
+    if spam == 3:
+        continue
+print ('spam is ' + str(spam))    
 
-#   For Loops
+#  For Loops
 
-# print ('my name is ')
-# for i in range(5):
-#     print('faiz five times ' + str(i))
+print ('my name is ')
+for i in range(5):
+    print('faiz five times ' + str(i))
     
-# name = 'faiz'
-# print('hello' + name)
+name = 'faiz'
+print('hello' + name)
 
 
-# num1 = 15
-# num2 = 25
-# print('the sum of two number is ' + str(num1 + num2) )
+num1 = 15
+num2 = 25
+print('the sum of two number is ' + str(num1 + num2) )
 
 
