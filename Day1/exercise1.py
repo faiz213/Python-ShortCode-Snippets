ddfc6c8361e4e77916cf754e6b71483ee331d2a1
faiz213 <bsef21m554@pucit.edu.pk>
2023-01-26 21:37:57 +0500

    Addded:

---
+++ Day1/exercise1.py
@@ -0,0 +1,13 @@
+#Write a program in main.py that prints the same notes from the previous lesson 
+# using what you have learnt about the Python print function.
+#example output :
+#  Day 1 - Python Print Function
+#The function is declared like this:
+#print('what to print')
+
+
+# Start
+
+print('Day 1 - Python Print Function')
+print('The function is declared like this : ')
+print("print ('what to print') ")
