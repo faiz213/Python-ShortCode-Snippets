704985192dfe417203bd82a48616c7516bae386d
faiz213 <bsef21m554@pucit.edu.pk>
2023-01-27 00:30:01 +0500

    Addded:

---
+++ Day1/Day1Project.py
@@ -0,0 +1,9 @@
+# here is a little project of Day 1 everything we learned today we will apply here and make little project
+#Project name is " Project Band Name Generator "
+
+print(" Welcome to Band Name Generator here you can simply give name of your city and your pet and genarate name of your band  ")
+
+city = input("what is your city name where you grew up : ")
+petName = input("what is your pet name : ")
+
+print ("your band name could be " + city + " " + petName )
