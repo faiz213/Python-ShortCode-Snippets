1469ef39ca6e617e8b3385de066227622b42d40b
faiz213 <bsef21m554@pucit.edu.pk>
2023-01-26 22:18:34 +0500

    Addded:

---
+++ Day1/exercise#2.py
@@ -0,0 +1,10 @@
+#Look at the code in the code editor on the right. There are errors in all of the lines of code. 
+# Fix the code so that it runs without errors.
+
+# Start
+
+print("Day 1 - String Manipulation") # in this line there was missing of double quotes
+print('String Concatenation is done with the"  "+" "sign.') # in this line we wanted to print actual "+" 
+#sign not the plus variable 
+print('e.g. print("Hello " + "world")') # in this line extra identation " ( " removed 
+print("New lines can be created with a backslash and n.") # and in this line in print function removed 
