052f7197b2c172af97ca10a581ac839f96095b93
faiz213 <bsef21m554@pucit.edu.pk>
2023-01-27 21:43:11 +0500

    Addded:

---
+++ Day2/excercise#1.py
@@ -0,0 +1,23 @@
+#Data Types
+
+# integer,string boolean and float  etc
+
+#type  function tells you the name of data type 
+#e.g
+
+#print(type("what is your name"))
+#print(type(13))
+
+
+
+# Write a program that adds the digits in a 2 digit number. 
+# e.g. if the input was 35, then the output should be 3 + 5 = 8.
+
+a = int(input("enter 1st number to add : "))
+b = int(input("enter 2nd number to add : "))
+
+c = a + b
+
+print(c)
+
+
