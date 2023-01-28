e731ab37ec9f0c663865c0728de2f49a2a62ca0d
faiz213 <bsef21m554@pucit.edu.pk>
2023-01-29 00:34:51 +0500

    Addded:

--- Day3/exercise#1.py
+++ Day3/exercise#1.py
@@ -1,7 +1,14 @@
+# LOOPS and NESTED LOOPS OVERVIEW
+
 print("wecome to the rollercoster!")
 height = int(input("please enter your height"))
 
 if height >= 150:
     print("you can ride the rollercoster!")
+    age = int(input("what is your age?"))
+    if age <= 18:
+        print("please pay $7")
+    else:
+        print("please pay $12")    
 else:
     print("your have to grow taller before you can ride. ")    
