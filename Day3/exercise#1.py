e7f8678a73f840aa86b822b197385bb5c41d7000
faiz213 <bsef21m554@pucit.edu.pk>
2023-01-28 23:31:48 +0500

    Addded:

---
+++ Day3/exercise#1.py
@@ -0,0 +1,7 @@
+print("wecome to the rollercoster!")
+height = int(input("please enter your height"))
+
+if height >= 150:
+    print("you can ride the rollercoster!")
+else:
+    print("your have to grow taller before you can ride. ")    
