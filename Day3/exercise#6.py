3947bfc14beffd33331fe9cc81e261a5ef346744
faiz213 <bsef21m554@pucit.edu.pk>
2023-01-31 22:34:12 +0500

    Addded:

---
+++ Day3/exercise#6.py
@@ -0,0 +1,34 @@
+# Love CALCULATOR 
+print("WELCOME TO THE LOVE CALCULATOR!!")
+name1 = input("what is your name ? \n")
+name2 = input("what is their name ? \n ")
+
+combined_string = name1 + name2
+
+lower_case_string=combined_string.lower()
+
+t = lower_case_string.count("t")
+r = lower_case_string.count("r")
+u = lower_case_string.count("u")
+e = lower_case_string.count("e")
+
+true = t + r + u + e
+
+l = lower_case_string.count("l")
+o = lower_case_string.count("o")
+v = lower_case_string.count("v")
+e = lower_case_string.count("e")
+
+love = l + o + v + e
+
+love_score = int(str(true) + str(love))
+
+if love_score < 10 or love_score > 90 :
+    print(f"your score is {love_score},you go toghether like coke and mentos ")    
+elif love_score >= 40 and love_score >= 50:
+    print(f"Your score is {love_score} , you are alright together.")
+else:
+    print(f"Your score is {love_score}.")    
+
+
+
