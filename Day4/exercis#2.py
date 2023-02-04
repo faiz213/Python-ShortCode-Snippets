96516419c5f6a7d8937d5c1160a7b1c813bb49d1
faiz213 <bsef21m554@pucit.edu.pk>
2023-02-04 23:57:45 +0500

    Addded:

---
+++ Day4/exercis#2.py
@@ -0,0 +1,7 @@
+#append function adds string after last value
+
+states_of_pakistan = ["Sindh","Panjab","Gilgit","Balochistan","Kashmir"]
+
+
+states_of_pakistan.append("Delhi")
+print(states_of_pakistan)
