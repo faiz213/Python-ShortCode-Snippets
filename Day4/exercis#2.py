427f818e9afa2df02d6c88bd976ae9097afa82b1
faiz213 <bsef21m554@pucit.edu.pk>
2023-02-05 00:52:40 +0500

    Addded:

--- Day4/exercis#2.py
+++ Day4/exercis#2.py
@@ -1,7 +1,16 @@
-#append function adds string after last value
+#You are going to write a program that will select a random name from a list of names.
+#  The person selected will have to pay for everybody's food bill.
 
-states_of_pakistan = ["Sindh","Panjab","Gilgit","Balochistan","Kashmir"]
+import random
 
+name_string = input("give me everybody's name separated by comma: ")
 
-states_of_pakistan.append("Delhi")
-print(states_of_pakistan)
+names = name_string.split(", ")
+
+num_items = len(names)
+
+random_choice = random.randint(0, num_items - 1)
+
+person_who_will_pay = names[random_choice]
+
+print(person_who_will_pay + " is going to buy the meal today")
