eb048c6251e4188a6cbf3bce1931ea0c05c15bca
faiz213 <bsef21m554@pucit.edu.pk>
2023-02-08 22:51:25 +0500

    Addded:

--- Day5/exercise#2.py
+++ Day5/exercise#2.py
@@ -1,10 +1,10 @@
 # 🚨 Don't change the code below 👇
 student_scores = input("Input a list of student scores ").split()
 for n in range(0, len(student_scores)):
-  student_scores[n] = int(student_scores[n])
+  student_scores[n] = str(student_scores[n])
 print(student_scores)
 # 🚨 Don't change the code above 👆
 
 #Write your code below this row 👇
 
-#print
+print(max(student_scores))
