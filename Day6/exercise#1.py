415ced53bfa3a482ddf37f0e29ece0cf09264682
faiz213 <bsef21m554@pucit.edu.pk>
2023-02-14 22:21:24 +0500

    Addded:

---
+++ Day6/exercise#1.py
@@ -0,0 +1,14 @@
+#defining functions and making of our own functions
+
+function_1 = len("hello world")
+
+print(function_1) #these are define function in python they are build in 
+
+# now we will create our own function to do that we use "def"
+
+def my_function():
+    print("hello faiz")
+    print("bye faiz ")
+
+my_function()  # after wrinting function we have to write it again in last 
+
